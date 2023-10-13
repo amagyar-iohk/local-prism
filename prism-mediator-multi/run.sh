fb0ade9092e6372663e96a6fda896047dbb9f955
Allain Magyar <allain.magyar@iohk.io>
2023-10-13 09:28:13 -0300

    ci: add script for 2 prim-agents

--- prism-mediator-multi/run.sh
+++ prism-mediator-multi/run.sh
@@ -2,7 +2,7 @@
 
 cleanup()
 {
-  ./stop.sh ${PRISM_PORT} ${MEDIATOR_PORT} ${DIDCOMM_PORT} ${COMPOSE_NAME}
+  ./stop.sh ${COMPOSE_NAME}
   exit 0
 }
 
@@ -12,14 +12,8 @@ fi
 
 ../update_env.sh
 
-PRISM_PORT="${PPORT:-8090}"
-MEDIATOR_PORT="${MPORT:-8080}"
-DIDCOMM_PORT="${DCPORT:-8081}"
 COMPOSE_NAME="local-prism-$RANDOM"
 
-MEDIATOR_PORT="$MEDIATOR_PORT" \
-PORT="$PRISM_PORT" \
-DIDCOMM_PORT="$DIDCOMM_PORT" \
 COMPOSE_NAME="$COMPOSE_NAME" \
 docker compose \
   -p "${COMPOSE_NAME}" \
