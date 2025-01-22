4c042bcb1554375cd495bca4f759ad38c119031b
Allain Magyar <allain.magyar@iohk.io>
2025-01-22 11:18:19 -0300

    changes
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

--- prism-mediator/run.sh
+++ prism-mediator/run.sh
@@ -2,7 +2,7 @@
 
 cleanup()
 {
-  ./stop.sh ${PRISM_PORT} ${MEDIATOR_PORT} ${DIDCOMM_PORT} ${COMPOSE_NAME}
+  ./stop.sh
   exit 0
 }
 
@@ -10,15 +10,6 @@ if [[ -z "${CI}" ]]; then
   trap cleanup SIGINT
 fi
 
-PRISM_PORT="${PPORT:-8090}"
-MEDIATOR_PORT="${MPORT:-8080}"
-DIDCOMM_PORT="${DCPORT:-8081}"
-COMPOSE_NAME="local-prism-$RANDOM"
-
-MEDIATOR_PORT="$MEDIATOR_PORT" \
-PORT="$PRISM_PORT" \
-DIDCOMM_PORT="$DIDCOMM_PORT" \
-COMPOSE_NAME="$COMPOSE_NAME" \
 docker compose \
   -p "${COMPOSE_NAME}" \
   -f "docker-compose.yml" \
--- prism-mediator/stop.sh
+++ prism-mediator/stop.sh
@@ -2,16 +2,7 @@
 
 set -e
 
-PRISM_PORT=$1
-MEDIATOR_PORT=$2
-DIDCOMM_PORT=$3
-COMPOSE_NAME=$4
-
-MEDIATOR_PORT="${MEDIATOR_PORT}" \
-PORT=${PRISM_PORT} \
-DIDCOMM_PORT=${DIDCOMM_PORT} \
-COMPOSE_NAME=${COMPOSE_NAME} \
 docker compose \
   -p "$COMPOSE_NAME" \
   -f "docker-compose.yml" \
-  --env-file ../.env down -v
+  --env-file ../.env down
