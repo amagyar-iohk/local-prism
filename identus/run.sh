cea8e0a4d512cdf9cd4b3c9558f681af77d7f89d
Allain Magyar <allain.magyar@iohk.io>
2025-04-11 08:59:55 -0300

    traefik
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

--- identus/run.sh
+++ identus/run.sh
@@ -2,7 +2,7 @@
 
 cleanup()
 {
-  ./stop.sh ${COMPOSE_NAME}
+  ./stop.sh
   exit 0
 }
 
@@ -10,14 +10,11 @@ if [[ -z "${CI}" ]]; then
   trap cleanup SIGINT
 fi
 
-COMPOSE_NAME="local-prism-$RANDOM"
-
-COMPOSE_NAME="$COMPOSE_NAME" \
 docker compose \
   -p "${COMPOSE_NAME}" \
   -f "docker-compose.yml" \
   --env-file ../.env up -d --wait
 
-echo
-echo "Press ctrl+c to end execution"
-read -r -d '' _ 
+# echo
+# echo "Press ctrl+c to end execution"
+# read -r -d '' _ 
